c52c3d9c2a63211d1f0eff46c88f67452d486e44
DIJAZ <3193289+cheesea3@users.noreply.github.com>
2021-07-25 19:48:23 -0400

    finished login device check, token regeneration, and cleaned requests

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,113 @@
-import './App.css';
-import Continue from './pages/Continue/Continue';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import Navbar from './components/Navbar/Navbar';
-import Home from './pages/Home/Home';
-import Start from './pages/Start/Start';
+import "./App.css";
+import Continue from "./pages/Continue/Continue";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Start from "./pages/Start/Start";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const getCookieValue = (name: string) =>
+  document.cookie.match("(^|;)\\s*" + name + "\\s*=\\s*([^;]+)")?.pop() || "";
 
 function App() {
+  const [isLoggedIn, updateLoggedIn] = useState(false);
+
+  function checkSession() {
+    var matchCookieOne = getCookieValue("POSTOGONID");
+    var matchCookieTwo = getCookieValue("POSTOGONID_");
+    if (matchCookieOne && matchCookieTwo) {
+      axios
+        .get("https://api.postogon.com/auth", {
+          params: {
+            token: matchCookieOne,
+          },
+        })
+        .then(function (response) {
+          updateLoggedIn(true);
+        })
+        .catch(function (error) {
+          console.log(matchCookieOne);
+          console.log(error);
+        });
+    }
+    //if matchCookieTwo doesn't exist then send delete request and regenerate a new token
+    else if (matchCookieOne && !matchCookieTwo) {
+      //get userid, create a new token with user id, set cookie for the userid with the new token, delete old token
+      //confirm token
+      axios
+        .get("https://api.postogon.com/auth", {
+          params: {
+            token: matchCookieOne,
+          },
+        })
+        .then(function (response) {
+          //create new token with existing userid
+          axios
+            .post(
+              "https://api.postogon.com/auth",
+              {},
+              {
+                params: {
+                  token: matchCookieOne,
+                },
+              }
+            )
+            .then(function (response) {
+              //update and or create cookies
+              const d = new Date();
+              d.setTime(d.getTime() + 7 * 24 * 60 * 60 * 1000);
+              let expires = "expires=" + d.toUTCString();
+              document.cookie =
+                "POSTOGONID=" +
+                Object.values(response.data) +
+                ";" +
+                expires +
+                ";path=/";
+              document.cookie = "POSTOGONID_=1;" + expires + ";path=/";
+              //delete old token
+              axios
+                .delete("https://api.postogon.com/auth", {
+                  params: {
+                    token: matchCookieOne,
+                  },
+                })
+                .then(function (response) {
+                  console.log("regenerated token");
+                  if (getCookieValue("POSTOGONID_")) {
+                    //validate new token
+                    axios
+                      .get("https://api.postogon.com/auth", {
+                        params: {
+                          token: getCookieValue("POSTOGONID"),
+                        },
+                      })
+                      .then(function (response) {
+                        updateLoggedIn(true);
+                      });
+                  }
+                });
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
+        })
+        .catch(function (error) {
+          console.log("token doesnt exist");
+          console.log(error);
+        });
+    }
+  }
+
+  useEffect(() => {
+    checkSession();
+    console.log(isLoggedIn);
+  }, [isLoggedIn]); // <-- empty dependency array
+
   return (
     <Router>
       <div>
-        {    <Navbar></Navbar>
-}
+        {<Navbar></Navbar>}
         <Switch>
           <Route path="/start">
             <Start></Start>
@@ -30,6 +123,5 @@ function App() {
     </Router>
   );
 }
-
 
 export default App;
