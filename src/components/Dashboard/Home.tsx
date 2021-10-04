326ede56f0ce70434c9a9c59eb685c90444a131f
Daiyaan Ijaz <3193289+cheesea3@users.noreply.github.com>
2021-10-03 23:04:10 -0400

    Design changes
    added public private buttons with design

--- src/components/Dashboard/Home.tsx
+++ src/components/Dashboard/Home.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { getUser, getEmail } from '../../Utils/Common'
 import Post from '../Feed/Post';
-
+import { LockOpenIcon as LockOOutline, LockClosedIcon as LockCOutline, LockClosedIcon } from '@heroicons/react/outline'
 
 const user = {
   name: getUser(),
@@ -22,14 +22,11 @@ const user = {
 <div>
 
 <div className="mt-4">
-    Posts
-    <br></br>
-   <h1 className="sr-only">Recent posts</h1>
-   <div>
-     
 
-<button onClick={()=> {hook("public")}}>Public Post</button>
-<button onClick={()=> {hook("private")}}>Private Post</button>
+   <h1 className="sr-only">Recent posts</h1>
+
+<div className="flex">
+{feed == "public" ? <div className="flex"><button className="hover:bg-gray-100 hover:text-blue-600 font-bold ripple-bg-white text-blue-500 select-none py-4 px-6 border-b border-gray-200 block cursor-default" onClick={()=> {hook("public")}}>Public</button> <button className="hover:bg-gray-100 hover:text-blue-600 py-4 px-6 text-gray-500 border-b border-gray-200 block select-none" onClick={()=> {hook("private")}}>Private</button></div> : <div className="flex"><button className="hover:bg-gray-100 hover:text-blue-600  py-4 px-6 text-gray-500 border-b border-gray-200 block" onClick={()=> {hook("public")}}>Public</button> <button className="hover:bg-gray-100 ripple-bg-white select-none  cursor-default hover:text-blue-600 font-bold text-blue-500 py-4 px-6 border-b border-gray-200 block select-none" onClick={()=> {hook("private")}}>Private</button></div>}
 
 </div>
 <Post feed={feed}></Post>
