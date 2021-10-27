8211bb1b45b6ce18f2edb02afaced3fbf25ad134
Daiyaan Ijaz <3193289+cheesea3@users.noreply.github.com>
2021-10-27 18:38:08 -0400

    functional login

--- src/pages/notLoggedIn/NotLoggedInNavSignIn.tsx
+++ src/pages/notLoggedIn/NotLoggedInNavSignIn.tsx
@@ -1,16 +1,42 @@
-import { IonBackButton, IonButton, IonButtons, IonContent, IonHeader, IonInput, IonItem, IonItemDivider, IonLabel, IonList, IonListHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonBackButton, IonButton, IonButtons, IonContent, IonHeader, IonInput, IonItem, IonItemDivider, IonLabel, IonList, IonListHeader, IonLoading, IonPage, IonTitle, IonToolbar, useIonAlert, useIonLoading } from '@ionic/react';
 import { SetStateAction, useState } from 'react';
+import axios from 'axios';
+
 
 const NotLoggedInNavSignIn: React.FC = () => {
 
+
+  const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState<string>();
   const [password, setPassword] = useState<string>();
+  const [present] = useIonAlert();
+  const [error, setError] = useState<String | null>(null);
 
-  function handleSubmit(){
-    console.log({email})
-    console.log({password})
-  }
+
+  // handle button click of login form
+  const handleLogin = () => {
+    setError(null);
+    setLoading(true);
+    axios.post('https://api.postogon.com/auth', { username: email, password: password }).then(response => {
+      setLoading(false);
+    }).catch(error => {
+      
+      setLoading(false);
+      if (error.response.status === 401) 
    
+      present({
+        header: 'There was a problem',
+        message: error.response.data,
+        buttons: [
+          { text: 'Ok', handler: (d) => console.log('Error acknowledged') },
+        ],
+        onDidDismiss: (e) => console.log('Error dismissed'),
+      })      
+      else 
+     setError("Something went wrong. Please try again later.");
+    });
+  }
+
     return (
       <IonPage>
       <IonHeader>
@@ -38,8 +64,17 @@ const NotLoggedInNavSignIn: React.FC = () => {
             <IonInput type="password" value={password} onIonChange={e => setPassword(e.detail.value!)}></IonInput>
           </IonItem> 
           <div className="flex justify-end mt-6">
-          <IonButton onClick={()=> handleSubmit}>Submit</IonButton>
-          </div>        
+          <IonButton 
+          onClick={() => {
+            handleLogin()}}  
+          >Submit</IonButton>
+      <IonLoading
+        isOpen={loading}
+        onDidDismiss={() => setLoading(false)}
+        message={'Please wait...'}
+      />          
+          </div>
+          <span className="mx-auto text-red-500 text-lg">{error}</span>
           </div>      
         </IonList>
       </IonContent>
