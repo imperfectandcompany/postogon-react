88821cb0288b88c818403777bb51f3850119d63b
Daiyaan Ijaz <3193289+cheesea3@users.noreply.github.com>
2021-12-08 01:42:34 -0500

    code refactor

--- src/pages/NotLoggedIn/NotLoggedInTOS.tsx
+++ src/pages/NotLoggedIn/NotLoggedInTOS.tsx
@@ -1,4 +1,4 @@
-import { IonBackButton, IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonInput, IonItem, IonItemDivider, IonLabel, IonList, IonListHeader, IonLoading, IonModal, IonPage, IonTitle, IonToolbar, useIonAlert, useIonLoading, useIonRouter, useIonToast } from '@ionic/react';
+import { IonBackButton, IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonInput, IonItem, IonItemDivider, IonLabel, IonList, IonListHeader, IonLoading, IonModal, IonPage, IonText, IonTitle, IonToolbar, useIonAlert, useIonLoading, useIonRouter, useIonToast } from '@ionic/react';
 import { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
 import { setLoginDetails } from '../../utils/Login';
@@ -11,13 +11,41 @@ import { chevronBack, chevronDown, chevronUp } from 'ionicons/icons';
 
 
 
+const Sections = [
+  { heading: 'Postogon', description: '“Postogon” refers to the platform on https://www.postogon.com and may not be confused as something else.' },
+  { heading: 'Imperfect and Company', description: '"Us", "We", "Our", "The company", all refers to Imperfect and Company as a group, as a company. We are not responsible for any mistakes you may have made with our identity.' },
+  { heading: 'Understand', description: 'By being on our platform, you understand that you are accountable for your own mistakes. You understand that if you do not represent the values that the company stands for then your account will be dealt with accordingly and as fairly as possible across all branches owned by us.  You understand that since Imperfect and Company operates and is hosted in the United States that the Electronic Signatures in Global and International Commerce Act (ESGICA) is in effect and failure to follow these rules will result in permanent removal from all branches of Imperfect and Company.' },
+  { heading: 'Acceptance', description: 'You hereby agree that by accepting this TOS, you know that your contract with Postogon also correlates any service that is owned by us.' },
+];
+
+const listSections = Sections.map((section) =>
+<div className="space-y-2 text-white">
+<div className="text-xl font-semibold md:text-xlg lg:text-3xl">
+<IonText>
+<h1>{section.heading}</h1>
+  </IonText>
+</div>
+<div className="text-sm md:text-md lg:text-lg">
+<IonText>
+<p>{section.description}</p>
+  </IonText>
+</div>
+
+
+
+</div>
+
+
+
+);
+
 function NotLoggedInNavTOS({ setShowTos, showTos }: any) {
 
   return (
 
     <IonModal isOpen={showTos} swipeToClose={true} cssClass={`${styles.modal}`} onDidDismiss={() => setShowTos(false)} >
 
-      <IonHeader>
+      <IonHeader collapse="condense">
         <IonToolbar className={`${styles['modal-toolbar']}`}>
           <IonButtons slot="start" className="cursor-pointer focus:cursor-default" onClick={() => setShowTos(false)}>
             <IonIcon slot="icon-only" icon={chevronDown} />
@@ -26,45 +54,15 @@ function NotLoggedInNavTOS({ setShowTos, showTos }: any) {
         </IonToolbar>
       </IonHeader>
 
-      <IonContent forceOverscroll={true} class={`${styles['modal-content']}`}>
+      <IonContent forceOverscroll={true} class={`${styles['modal-content']}`} >
         <div className="flex flex-col space-y-6 p-6 text-white text-left tracking-wide leading-relaxed">
           <div>
-            <p className="text-lg lg:text-2xl mb-6">
+            <p className="text-lg md:text-xl text-center lg:text-2xl mb-6">
               Please understand the following Terms of Service in which you are in agreement with by accessing or
               registering on Postogon.</p>
           </div>
-          <div className="flex flex-col space-y-2">
-            <div className=" text-xl font-semibold lg:text-3xl">
-              Postogon
-            </div>
-            <div className="text-sm">
-              “Postogon” refers to the platform on https://www.postogon.com and may not be confused as something else.
-            </div>
-          </div>
-          <div className="flex flex-col space-y-2">
-            <div className="text-xl font-semibold lg:text-3xl">
-              Imperfect and Company
-            </div>
-            <div className="text-sm">
-              "Us", "We", "Our", "The company", all refers to Imperfect and Company as a group, as a company. We are not responsible for any mistakes you may have made with our identity.
-            </div>
-          </div>
-          <div className="flex flex-col space-y-2">
-            <div className="text-xl font-semibold lg:text-3xl">
-              Understand
-            </div>
-            <div className="text-sm">
-              By being on our platform, you understand that you are accountable for your own mistakes. You understand that if you do not represent the values that the company stands for then your account will be dealt with accordingly and as fairly as possible across all branches owned by us.  You understand that since Imperfect and Company operates and is hosted in the United States that the Electronic Signatures in Global and International Commerce Act (ESGICA) is in effect and failure to follow these rules will result in permanent removal from all branches of Imperfect and Company.
-            </div>
-          </div>
-          <div className="flex flex-col space-y-2">
-            <div className="text-xl font-semibold lg:text-3xl">
-              Acceptance
-            </div>
-            <div className="text-sm">
-              You hereby agree that by accepting this TOS, you know that your contract with Postogon also correlates any service that is owned by us.
-            </div>
-          </div>
+          {listSections}
+
         </div>
       </IonContent>
 
--- src/pages/notLoggedIn/NotLoggedInNav.tsx
+++ src/pages/notLoggedIn/NotLoggedInNav.tsx
@@ -1,4 +1,4 @@
-import { IonButton, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonModal, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonModal, IonPage, IonText, IonTitle, IonToolbar } from '@ionic/react';
 import { mail, star } from 'ionicons/icons';
 import { useState } from 'react';
 import styles from './NotLoggedInNav.module.css'; // Import css modules stylesheet as styles
@@ -10,45 +10,45 @@ const NotLoggedInNav: React.FC = () => {
   const [showTos, setShowTos] = useState(false);
 
   return (
-    <IonPage className={`${styles.bg}`}>
+    <IonPage  className={`${styles.bg}`}>
       <IonContent >
-
+      <IonHeader class="ion-no-border" className={`${styles.text}`}  collapse="condense" >
+        <div className="flex space-x-4 ion-justify-content-center ion-align-items-center mt-12 mb-14 text-4xl sm:text-6xl md:text-8xl lg:text-8xl font-bold">
+          <img src={process.env.PUBLIC_URL + '/assets/icon/icon.svg'} className="w-12 sm:w-24 md:w-32 lg:w-36" alt="image" />
+          <div>Postogon</div>
+        </div>
+      </IonHeader>
         <NotLoggedInNavTOS showTos={showTos} setShowTos={setShowTos} />
 
 
-        <div className="md:row-start-1 md:col-start-2 md:col-end-2 p-12">
-          <div className="flex flex-col justify-center h-full">
-
-            <div className="flex space-x-4 justify-center items-center text-4xl lg:text-6xl mt-12 mb-14">
-              <img src={process.env.PUBLIC_URL + '/assets/icon/icon.svg'} className="w-16 md:w-32" alt="image" />
-              <div className="text-white text-center text-4xl lg:text-7xl font-bold" slot="fixed">Postogon</div>
-            </div>
-
-            <h1 className="inline-block tracking-wider align-top text-white text-center text-3xl lg:text-6xl font-bold mt-24 mb-20" slot="fixed">
+        <div className="p-12">
+          <div className="flex flex-col justify-center">
+            <h1 className="inline-block tracking-wider align-top text-white text-center text-3xl lg:text-6xl font-bold mt-6 md:mt-24 mb-20">
               Be yourself here and relax.
             </h1>
 
-            <div className="flex flex-col" slot="fixed">
-              <div>
-                <IonButton className="flex font-bold" color="white" fill="outline" routerLink="signin">
-                  <IonIcon slot="start" icon={mail} />
+            <div className="ion-margin-bottom">
+              <IonButton className="flex font-bold" color="white" fill="outline" routerLink="signin">
+              <IonIcon slot="start" icon={mail} />
                   Sign in with email
-                </IonButton>
-              </div>
-              <div className="mt-4 mb-2">
-                <IonButton className="flex font-bold" color="white" fill="clear" routerLink="signup">New account</IonButton>
-              </div>
+              </IonButton>
+              <IonButton className="flex font-bold" color="white" fill="clear" routerLink="signup">New account</IonButton>
+
+              
             </div>
 
-            <div className="mt-6">
-              <div className="flex items-start justify-start">
-                <p className="flex-1 text-xs tracking-wider leading-tight text-center text-white lg:text-2xl mb-6" slot="fixed">
-                  By continuing, you agree to our <button className="underline select-none focus:select-none cursor-pointer hover:text-gray-100 focus:text-opacity-50 transition" onClick={() => setShowTos(true)}>Terms of Service</button>. We commit to protecting your data, read more about our <span className="underline">Privacy Policy</span> and <span className="underline">Cookies Policy</span>.
+            <IonLabel className="ion-margin-top">
+              <IonText color="white">
+                <p className="ion-text-center ion-margin-vertical">
+                  By continuing, you agree to our <button className="underline select-none focus:select-none cursor-pointer hover:text-gray-100 focus:text-opacity-50 transition" onClick={() => setShowTos(true)}>Terms of Service</button>.
+                  We commit to protecting your data, read more about our <span className="underline">Privacy Policy</span> and <span className="underline">Cookies Policy</span>.
                 </p>
-              </div>
-              <button className="flex mx-auto text-white text-center text-lg lg:text-2xl font-bold" slot="fixed">
+              </IonText>
+            </IonLabel>
+            <div className="ion-text-center">
+              <IonButton fill="clear" color="white" className=" lg:text-2xl font-bold" >
                 Trouble getting in?
-              </button>
+              </IonButton>
             </div>
           </div>
         </div>
