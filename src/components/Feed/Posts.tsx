66d40d2db53228ee1a87e1ea94619a0453219034
Daiyaan Ijaz <3193289+cheesea3@users.noreply.github.com>
2022-01-24 19:16:53 -0500

    RTK implemented (without pagination)
    In the future use this implementation, but also add endpoint that fetches data based on specific page.

--- src/components/Feed/Posts.tsx
+++ src/components/Feed/Posts.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { fetchPosts, fetchPostsFeed, fetchPostsType, initialState, IPost, loadPosts, startLoading, stopLoading, updateLike, updatePosition } from '../../features/post/postSlice';
+import React, { useEffect, useMemo, useState } from 'react';
+import { fetchPosts, fetchPostsFeed, fetchPostsType, fetchSuccess, initialState, IPost, loadPosts, startLoading, stopLoading, updateLike, updatePosition } from '../../features/post/postSlice';
 import SinglePost from './Post/SinglePost';
 import api from "../../utils/API";
 import { getToken } from '../../utils/Common';
@@ -10,6 +10,9 @@ import Loading from '../Loading/Loading';
 import CreatePost from '../Timeline/CreatePost';
 import MoreOptions from './MoreOptions';
 import { bookmarkOutline, paperPlaneOutline, chatbubblesOutline, heart, heartOutline, chevronDownCircleOutline, arrowDownCircle, chevronDownCircle, chevronDownCircleSharp } from 'ionicons/icons';
+import { useGetTimelineFeedQuery } from '../../features/api/apiSlice';
+import Post from '../Timeline/Post';
+import { setMaxListeners } from 'process';
 
 
 
@@ -22,52 +25,27 @@ interface PostProps {
 
 function Posts(props: PostProps) {
 
-    const loadedPosts = useAppSelector(state => state.post.loadedPosts)
-    const posts = useAppSelector(state => state.post.posts)
-    const isLoading = useAppSelector(state => state.post.isLoading)
-    const [isInfiniteDisabled, setInfiniteDisabled] = useState(false);
-    const [isLiked, setIsLiked] = useState(false);
+    const {
+        data: posts = [],
+        isLoading,
+        isFetching,
+        isSuccess,
+        isError,
+        error,
+        refetch
+      } = useGetTimelineFeedQuery(props.feed);
 
 
+      function doRefresh(event: CustomEvent<RefresherEventDetail>) {
+        refetch();
+        setTimeout(() => {
+          event.detail.complete();
+        }, 1000);
+      }
 
-    
-    const dispatch = useAppDispatch();
 
-    
       
-    //load more posts
-    const morePosts = () => {
-        setTimeout(() => {
-            dispatch(loadPosts());
-            dispatch(stopLoading());            
-        }, 0);
-        dispatch(updatePosition());
-    };
-
-    const loadData = (ev: any) => {
-        dispatch(startLoading());
-        setTimeout(() => {
-            morePosts();
-            console.log('Loaded data');
-            ev.target.complete();
-            if (posts.length === loadedPosts.length-8) {
-                setInfiniteDisabled(true);
-            }
-        }, 0);
-    }
-
-    function doRefresh(event: CustomEvent<RefresherEventDetail>) {
-        //force refetch of posts, this called function itializes with isLoading as true and finishes with isLoading as false.
-        dispatch(fetchPosts(props.type, props.feed));
-        setTimeout(() => {
-            event.detail.complete();
-        }, 500);
-    }
-
-    //fetch posts based on if the feed changes, this called function itializes with isLoading as true and finishes with isLoading as false.
-    useEffect(() => {
-        dispatch(fetchPosts(props.type, props.feed));
-    }, [props.feed]);
+      let content
 
     const loadSkeletonHeader = (title:string) => {
         return (
@@ -76,16 +54,6 @@ function Posts(props: PostProps) {
                     <div className=" custom-skeleton">
                         <IonSkeletonText animated style={{ width: '37%' }} />
                     </div>
-                </IonLabel>
-            </IonListHeader>
-        )
-    }
-
-    const loadHeader = (title:string) => {
-        return (
-            <IonListHeader>
-                <IonLabel>
-                {title}
                 </IonLabel>
             </IonListHeader>
         )
@@ -112,57 +80,40 @@ function Posts(props: PostProps) {
       }
 
 
-    //if loading is true and post type is not id
-    if (isLoading && !props.id && loadedPosts.length <= 0) {
-        return loadSkeletonView(8, "Recent posts");
-    }
-
-    //if loading is true and post type is id
-    if (isLoading && props.id && loadedPosts.length <= 0) {
-        return loadSkeletonView(1, "Post");
-    }
+      const sortedPosts = useMemo(() => {
+        const sortedPosts = posts.slice()
+        // Sort posts in descending chronological order
+        sortedPosts.sort((x, y) => y.PostedOn - x.PostedOn)
+        return sortedPosts
+      }, [posts]);
 
 
 
-    const AddPostToLikes = (e: React.MouseEvent<HTMLIonIconElement, MouseEvent> | React.MouseEvent<HTMLIonButtonElement, MouseEvent>, feed: string, postID: string,) => {
-        e.stopPropagation();
-        //add post like to backend...
-        setIsLiked(isLiked ? false : true);
-    }
-
-
+    if (isLoading) {
+        content = loadSkeletonView(1, "Still Loading");
+      } else if (isSuccess) {
+        const renderedPosts = sortedPosts.map((post: IPost, index: number) => (
+            <SinglePost key={index} PostedOn={post.PostedOn} PostBody={post.PostBody} PostedBy={post.PostedBy} Likes={post.Likes} PostId={post.PostId} />
+        ))
+        content = <div ><IonList className="relative bg-white divide-y shadow-lg divide-gray-50">{renderedPosts}              </IonList></div>
+      } else if (isError) {
+        content = <div>{error?.toString()}</div>
+      }
     return (
         <React.Fragment>
-
-
             <div>
-                <ul>
-                    <IonRefresher slot="fixed" onIonRefresh={doRefresh}>
-                    <IonRefresherContent
-          pullingIcon={arrowDownCircle}
-          pullingText="Pull to refresh"
-          refreshingSpinner="dots"
-          refreshingText="Refreshing...">
-        </IonRefresherContent>
-                    </IonRefresher>
-                    <IonList className="relative bg-white divide-y shadow-lg divide-gray-50">
-                        <div className="snap-y snap-mandatory">
-                            {isLoading ? null: loadHeader("Recent posts")}
-                            {posts && posts.length > 0 && !isLoading ? loadedPosts.map((post: IPost, index: number) =>
-                              <SinglePost key={index} PostedOn={post.PostedOn} PostBody={post.PostBody} PostedBy={post.PostedBy} Likes={post.Likes} PostId={post.PostId} />
-                           ) : loadSkeletonView(loadedPosts.length+8, "Still Loading")}
-                        </div>
-                    </IonList>
-                    <IonInfiniteScroll
-                        onIonInfinite={loadData}
-                        threshold="99%"
-                        disabled={isInfiniteDisabled}
-                    >
-                        <IonInfiniteScrollContent
-                            loadingSpinner="dots"
-                        ></IonInfiniteScrollContent>
-                    </IonInfiniteScroll>
-                </ul>
+            <ul>
+            <IonRefresher slot="fixed" disabled={isFetching} onIonRefresh={doRefresh}>
+        <IonRefresherContent
+pullingIcon={arrowDownCircle}
+pullingText="Pull to refresh"
+refreshingSpinner="dots"
+refreshingText="Refreshing...">
+</IonRefresherContent>
+        </IonRefresher>
+            {content}
+            </ul>
+
             </div></React.Fragment>
     );
 }
